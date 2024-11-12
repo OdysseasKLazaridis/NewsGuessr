f24fbf0a61671524b0b2886b4aeca84f47e8dd78
OdysseasKLazaridis <odysseaskyriakos@gmail.com>
2024-11-13 00:44:32 +0100

    Newsscrapping
    the newscraping works.
    everything works with lower case
    we have some lame words that we discard. need to make the game

--- Web_Scraping/main.py
+++ Web_Scraping/main.py
@@ -10,15 +10,27 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
+from tqdm import tqdm
 
 
 def get_article_text(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     paragraphs = soup.find_all('p')
-    print(paragraphs)
     return ' '.join([para.get_text() for para in paragraphs])
+
+def unique_urls(urls):
+    unique_urls = []
+    seen = set()
+
+    for url in urls:
+        if url not in seen:
+            unique_urls.append(url)
+            seen.add(url)
+    return unique_urls
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -38,68 +50,91 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts)  
     return u" ".join(t.strip() for t in visible_texts)
 
+def process_article(url, driver, class_type, keyword,lame_words):
+    keyword = keyword.lower()
+    lower_case_lame_words = [word.lower() for word in lame_words]
+        # Check if lame_words is None
+    if lame_words is None:
+        print("Error: lame_words is None")
+        return
+    try:
+        driver.get(url)
+        html = driver.page_source
+        sentences = nltk.sent_tokenize(text_from_specific_class(html,class_type))
+        
+        for sentence in sentences:
+            # Tokenize each sentence into words
+            words = nltk.word_tokenize(sentence)
+            # Flatten the list if you want a single list of all words
+            lower_case_words = [word.lower() for word in words]
+            if keyword in lower_case_words:
+                for word in lower_case_words:
+
+                    if len(word)>2 and word not in lower_case_lame_words:
+
+                        if word in all_words:
+                            all_words_count[all_words.index(word)] += 1
+                        else:
+                            all_words.append(word)
+                            all_words_count.append(1)
+    except WebDriverException as e:
+        # Catch errors like net::ERR_NAME_NOT_RESOLVED and others
+        print(f"Error occurred while accessing {url}: {str(e)}")
+    except Exception as e:
+        # Handle other unexpected exceptions
+        print(f"An unexpected error occurred: {str(e)}")
+
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
+
 keyword = "Trump"
-page_number = 1
+
+lame_words =  ["mr.","the", "and","for","not","was","they","who","from","will","would","The","had","been","what","also","him","more","which","his","has","with","donald","are","with","their","all","have","that","ms.","when","she","out"]
+lame_words = [keyword] + lame_words
+page_number = 10
 nouns = []
 nouns_count = []
 count = 0
 urls_num = 0
+all_words = []
+all_words_count = []
 
-for i in range(2):
-    
 
-    url = "https://www.nytimes.com/section/us?page="+str(page_number)
-    class_type = "css-at9mc1 evys1bk0"
+base_urls = ["https://www.nytimes.com/section/us","https://www.nytimes.com/section/world","https://www.nytimes.com/section/politics","https://www.nytimes.com/section/nyregion","https://www.nytimes.com/spotlight/california-news",
+             "https://www.nytimes.com/section/climate","https://www.nytimes.com/section/business","https://www.nytimes.com/spotlight/donald-trump"]
+class_type = "css-at9mc1 evys1bk0"
+
+urls = []
+# Request for article URLs
+for base_url in base_urls:
+    url = f"{base_url}?page={1}"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
-    request = requests.get(url,headers)
+    request = requests.get(url, headers=headers)
     soup = BeautifulSoup(request.text, 'html.parser')
-    urls = soup.find_all('a', href=True)  # href=True ensures we only get <a> tags with href attributes
-
-    # Filter links that contain '2024/10'
-    filtered_urls = [url['href'] for url in urls if '2024/' in url['href']]
-    urls_num = len(filtered_urls)
-    final_urls = ["https://www.nytimes.com" + item for item in filtered_urls]
-    print(final_urls)
-
-    for url in final_urls:
-        driver.get(url)
-        html = driver.page_source
-
-        # time.sleep(4)
-        # # Create a request object with the URL and headers
-        # request = urllib.request.Request(url, headers=headers)
-
-        # # Open the URL with the request object
-        # html = requests.get(url,headers=headers).text
-        print(html)
-        sentences = nltk.sent_tokenize(text_from_specific_class(html,class_type))
 
 
-        for sentence in sentences:
-            # Tokenize each sentence into words
-            words = nltk.word_tokenize(sentence)
+    # Find URLs of individual articles
+    urls = urls+([f"https://www.nytimes.com{a['href']}" for a in soup.find_all('a', href=True) if '2024/' in a['href']])
+print(urls)
+unique_urls = unique_urls(urls)
+failed_urls = 0
 
-            # Flatten the list if you want a single list of all words
-            all_words = [word for sentence in words for word in sentence]
-            if keyword in all_words:
-                blob = TextBlob(sentence)
-                blob_nouns = blob.noun_phrases
-                for noun_phrase in blob_nouns:
-                    if noun_phrase in nouns:
-                        nouns_count[nouns.index(noun_phrase)] += 1
-                    else:
-                        nouns.append(noun_phrase)
-                        nouns_count.append(1)
+for url in tqdm(unique_urls):
+    try:
+        process_article(url, driver, class_type, keyword,lame_words)
+        # You can process the result here (e.g., store it in a list, process further, etc.)
+        # Assuming you want to append the results to a list:
+    except Exception as e:
+        print(f"Error processing {url}: {str(e)}")
+ 
 
 # Save the arrays
-np.savez('nouns_data.npz', nouns=nouns, nouns_count=nouns_count)
+np.savez('all_words.npz', words=all_words, words_count=all_words_count)
 
-sorted_pairs = sorted(zip(nouns, nouns_count), reverse=True)
+sorted_pairs = sorted(zip(all_words, all_words_count), reverse=True)
 
 for string, number in sorted_pairs:
     print(f"{string}: {number}")
 
-print(str(urls_num)+ " articles were scrapped")
+print(str(len(urls))+ " articles were scrapped")
--- Web_Scraping/visualize_data.py
+++ Web_Scraping/visualize_data.py
@@ -1,20 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+file_name = "all_words"+".npz"
 # Load the data from a .npz file
-data = np.load('nouns_data.npz')
-nouns = data['nouns']
-nouns_count = data['nouns_count']
-print(nouns)
-print(nouns_count)
+data = np.load(file_name)
+words = data['words']
+count = data['words_count']
+
+
+# Get indices for sorting in descending order of counts
+sorted_indices = np.argsort(count)[::-1]
+
+# Sort words and counts based on those indices
+sorted_words = words[sorted_indices]
+sorted_counts = count[sorted_indices]
+
+# Print the sorted results
+print("Sorted words:", sorted_words)
+print("Sorted counts:", sorted_counts)
+
 # Create a bar chart
 plt.figure(figsize=(10, 6))
-plt.bar(nouns, nouns_count, color='skyblue')
+plt.bar(sorted_words[:50], sorted_counts[:50], color='skyblue')
 
 # Add title and labels
 plt.title('Nouns Frequency', fontsize=16)
-plt.xlabel('Nouns', fontsize=12)
-plt.ylabel('Count', fontsize=12)
+plt.xlabel('words', fontsize=12)
+plt.ylabel('words_count', fontsize=12)
 
 # Rotate the x-axis labels for readability
 plt.xticks(rotation=45, ha='right')
